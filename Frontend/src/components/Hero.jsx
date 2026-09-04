7a8362f23996566ed675d1facf129c90be43105a
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-05 02:45:42 +0530

    Refactor styling for Hero, Navbar, and Tittle components; adjust height properties for better consistency

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
             <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0.5'>
                 <div className='text-[#414141]'>
                     <div className='flex items-center gap-2'>
-                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
                         <p className='font-medium tex-sm md:text-base'>OUR BESTSELLERS</p>
                     </div>
                     <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leadinf-relaxed'>Latest Arrivals</h1>
                     <div className='flex items-center gap-2'>
                         <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
-                        <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
+                        <p className='w-8 md:w-11 h-px bg-[#414141]'></p>
                     </div>
                 </div>
             </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+                    <p className='absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
                 </Link>
                 <img onClick={() => { setVisible(true) }} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
             </div>
