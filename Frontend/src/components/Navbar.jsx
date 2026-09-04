420dc511d43a2d2dbc242b971a26e1068bc1ea50
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-04 22:39:55 +0530

    Add initial frontend assets and components
    - Added various PNG assets including icons and images for the frontend.
    - Created initial React components: Hero, Navbar, About, Cart, Collection, Contact, Home, Login, Orders, PlaceOrder, Product.
    - Set up main entry point with React Router and Tailwind CSS integration.
    - Included a basic CSS file for styling.
    - Configured Vite for the project.

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+
+    const [visible, setVisible] = useState(false)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Navbar
