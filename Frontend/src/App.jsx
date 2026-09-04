20dfb34e89daf21e7b0319d2cd1dcbb4717c107d
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-05 02:40:11 +0530

    Fix routing for PlaceOrder component and update Navbar for logo link; enhance CSS for active links and add LatestCollection to Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Route path='/product/:product_Id' element={<Product />}/>
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/place-order' element={PlaceOrder} />
+        <Route path='/place-order' element={<PlaceOrder />} />
         <Route path='/orders' element={<Orders />} />
       </Routes>
     </div>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
-            <img src={assets.logo} className='w-36' alt="" />
+            <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to='/Home' className='flex flex-col items-center gap-1'>
                     <p>Home</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
                 </NavLink>
                 <NavLink to='/Collection' className='flex flex-col items-center gap-1'>
                     <p>Collection</p>
