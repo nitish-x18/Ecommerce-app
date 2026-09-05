ceee5f6c55dcd8cab5eacc5d2811a5dbcaf42545
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-05 23:31:27 +0530

    Add SearchBar component and integrate search functionality in Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/frontend_assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Navbar = () => {
 
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
+
+    const {setShowSearch} = useContext(ShopContext);
 
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
@@ -14,7 +17,7 @@ const Navbar = () => {
                     <p>Home</p>
                     <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
                 </NavLink>
-                <NavLink to='/Collection' className='flex flex-col items-center gap-1'>
+                <NavLink to='/collection' className='flex flex-col items-center gap-1'>
                     <p>Collection</p>
                     <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
                 </NavLink>
@@ -30,7 +33,7 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+                <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='group relative'>
                     <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
