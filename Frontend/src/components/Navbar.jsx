12a5e25099dfff9dff1f67ceb362912d729305b6
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-05 16:55:48 +0530

    Update Navbar component to correct navigation link and hide horizontal rule

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
         <div className='flex items-center justify-between py-5 font-medium'>
             <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-                <NavLink to='/Home' className='flex flex-col items-center gap-1'>
+                <NavLink to='/' className='flex flex-col items-center gap-1'>
                     <p>Home</p>
-                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                    <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
                 </NavLink>
                 <NavLink to='/Collection' className='flex flex-col items-center gap-1'>
                     <p>Collection</p>
