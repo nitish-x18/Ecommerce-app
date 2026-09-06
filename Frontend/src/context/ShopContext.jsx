fea8fa6ca58b9c3bae792d8d3976bb7ee73e5bae
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-06 14:26:38 +0530

    Add cart functionality and toast notifications for size selection errors

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,10 +10,53 @@ const ShopContextProvider = (props)=>{
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async(itemId, size) => {
+
+        if(!size){
+            toast.error('Select product size')
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const itmes in cartItems){
+            for(const item in cartItems[itmes]){
+                try{
+                    if(cartItems[itmes][item] > 0){
+                        totalCount += cartItems[itmes][item];
+                    }
+                } catch(error){
+                    console.log(error)
+                }
+            }
+        }
+        return totalCount;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart,
+        getCartCount
     }
 
     return (
