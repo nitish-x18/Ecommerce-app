2d35330a4069edf13ab1710e71dc0d02e27c5c57
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-07 16:11:49 +0530

    Initialize server with Express, CORS, and MongoDB connection

---
+++ Backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from '../src/config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// APP CONFIG----->
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// MIDDLEWARES----->
+app.use(express.json())
+app.use(cors());
+
+// API ENDPOINTS---->
+app.get('/', (req, res)=>{
+    res.send("API WORKING");
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started on PORT: http://localhost:${port}`)
+});
