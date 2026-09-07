2ae29f1e47a6bfb1126c65f56ce2d26dd1b9b3a7
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-07 21:05:05 +0530

    Fix formatting by adding a newline at the end of the singleProduct function

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -96,7 +96,7 @@ const removeProduct = async (req, res) => {
 
 //function for single product info
 const singleProduct = async (req, res) => {
-
+    
 }
 
 export {listProduct, removeProduct, addProduct, singleProduct};
