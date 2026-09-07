76532febdadc740b4fc5484f069445f1ed8c50a1
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-07 20:44:13 +0530

    Add product routes for managing products in the API

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from '../src/config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/user.route.js';
+import productRouter from './routes/product.route.js';
 
 // APP CONFIG----->
 const app = express();
@@ -17,6 +18,7 @@ app.use(cors());
 
 // API ENDPOINTS---->
 app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
 
 app.get('/', (req, res)=>{
     res.send("API WORKING");
