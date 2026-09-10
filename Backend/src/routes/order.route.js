2495eff1f7c3cdd251b1bc57634aab69cd72fe0d
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-10 20:41:09 +0530

    feat: implement order routing for managing orders and payment processing

---
+++ Backend/src/routes/order.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus } from '../controllers/order.controller.js';
+import adminAuth from '../middleware/adminAuth.middleware.js';
+import authUser from '../middleware/auth.middleware.js';
+
+const orderRouter = express.Router();
+
+// ADMIN FETURES--->>
+orderRouter.post('list',adminAuth ,allOrders);
+orderRouter.post('/status',adminAuth ,updateStatus);
+
+// PAYMENT FEATURES-->>
+orderRouter.post('/place',authUser, placeOrder);
+orderRouter.post('/stripe',authUser, placeOrderStripe);
+orderRouter.post('/razorpay',authUser, placeOrderRazorpay);
+
+// USER FEATURES--->>>
+orderRouter.post('/userorders', authUser, userOrders);
+
+export default orderRouter;
