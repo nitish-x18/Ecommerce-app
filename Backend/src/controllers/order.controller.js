29b723400ef26f1930bb2dac9a88b69ef9f8a3d7
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-10 20:41:22 +0530

    feat: implement order controller with methods for various payment options and order management

---
+++ Backend/src/controllers/order.controller.js
@@ -0,0 +1,33 @@
+
+
+// PLACING ORDER USING COD METHOD--->>
+const placeOrder = async (req, res) => {
+
+}
+
+// PLACING ORDER USING STRIPE METHOD--->>
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// PLACING ORDER USING RAZORPAY METHOD--->>
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// ALL ORDER DATA FRO ADMIN PANEL--->>
+const allOrders = async(req, res) => {
+
+}
+
+// USER ORDER DATA FOR FRONTEND--->>
+const userOrders = async (req, res) => {
+
+}
+
+// UPDATE ORDER STATUS FROM ADMIN PANEL--->>
+const updateStatus = async (req, res) => {
+
+}
+
+export { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus }
