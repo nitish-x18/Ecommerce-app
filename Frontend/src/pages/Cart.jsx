480b0b35f7dd2a3963e5f81ba140b33d2a632108
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-10 20:10:02 +0530

    refactor: clean up Cart component code for readability and consistency

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -6,35 +6,39 @@ import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const {products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
-  useEffect(()=>{
-    const tempData = [];
-    for(const items in cartItems){
-      for(const item in cartItems[items]){
-        if(cartItems[items][item] > 0){
-          tempData.push({
-            _id: items,
-            size: item,
-            quantity: cartItems[items][item]
-          })
+  useEffect(() => {
+
+    if (products.length > 0) {
+      const tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item]
+            })
+          }
         }
       }
+      setCartData(tempData);
     }
-    setCartData(tempData);
-  },[cartItems])
+
+  }, [cartItems, products])
 
   return (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>
-        <Tittle text1={'YOUR'} text2={'CART'}/>
+        <Tittle text1={'YOUR'} text2={'CART'} />
       </div>
       <div>
         {
-          cartData.map((item,idx)=>{
-            const productData = products.find((product)=> product._id == item._id)
+          cartData.map((item, idx) => {
+            const productData = products.find((product) => product._id == item._id)
             return (
               <div key={idx} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4' >
                 <div className='flex items-start gap-6'>
@@ -47,8 +51,8 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <input onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.size,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type='number' min={1} defaultValue={item.quantity} />
-                <img onClick={()=>updateQuantity(item._id,item.size,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type='number' min={1} defaultValue={item.quantity} />
+                <img onClick={() => updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             )
           })
@@ -58,7 +62,7 @@ const Cart = () => {
         <div className='w-full sm:w-112.5'>
           <CartTotal />
           <div className='w-full text-end'>
-            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 py-3 px-8'>PROCEED TO CHECKOUT</button>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 py-3 px-8'>PROCEED TO CHECKOUT</button>
           </div>
         </div>
       </div>
