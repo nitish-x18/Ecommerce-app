878915f323b6fb5707503deec31b8defbcb234f3
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-10 19:19:38 +0530

    feat: add cart API endpoint to server configuration

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,6 +5,7 @@ import connectDB from '../src/config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/user.route.js';
 import productRouter from './routes/product.route.js';
+import cartRouter from './routes/cart.route.js';
 
 // APP CONFIG----->
 const app = express();
@@ -19,6 +20,7 @@ app.use(cors());
 // API ENDPOINTS---->
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
 
 app.get('/', (req, res)=>{
     res.send("API WORKING");
