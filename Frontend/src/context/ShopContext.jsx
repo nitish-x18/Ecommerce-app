671e9b907f13f2e2c139c3d349e401b3ae84f078
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-10 20:10:09 +0530

    feat: implement cart synchronization with backend for add, update, and fetch operations

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -40,6 +40,17 @@ const ShopContextProvider = (props)=>{
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if(token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/add', {itemId, size}, {headers: {token}})
+                
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -63,6 +74,17 @@ const ShopContextProvider = (props)=>{
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData);
+
+        if(token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers: {token}})
+                
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartAmount = ()=>{
@@ -99,6 +121,20 @@ const ShopContextProvider = (props)=>{
         }
     }
 
+    const getUserCart = async ( token ) => {
+        try {
+
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}})
+            if(response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+            
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    } 
+
     useEffect(()=>{
         getProductData();
     },[])
@@ -106,6 +142,7 @@ const ShopContextProvider = (props)=>{
     useEffect(()=>{
         if(!token && localStorage.getItem('token')){
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
     },[])
 
