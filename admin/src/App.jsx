8753d0378cfc32d92e8b40c48c0c49a2f881f4f0
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-11 16:19:42 +0530

    fix: correct route path for Order component in App

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
               <Routes>
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
-                <Route path='/add' element={<Order token={token} />} />
+                <Route path='/orders' element={<Order token={token} />} />
               </Routes>
             </div>
           </div>
