1071a794cfaa0378e16bf38d647e5c1725283bff
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-11 23:51:56 +0530

    fix: correct grid class name formatting in Order component

--- admin/src/pages/Order.jsx
+++ admin/src/pages/Order.jsx
@@ -55,7 +55,7 @@ const Order = ({ token }) => {
       <div>
         {
           orders.map((order, idx) => (
-            <div className='grid grid-cols-1 sm:grid grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={idx}>
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={idx}>
               <img className='w-12' src={assets.parcel_icon} alt="" />
               <div>
                 <div>
