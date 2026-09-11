6d3a8294c244f02ff789ae0aba1ce3f70bebac8f
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-11 22:42:08 +0530

    feat: implement Stripe verification functionality in verifyStripe method

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -111,6 +111,36 @@ const placeOrderStripe = async (req, res) => {
 
 }
 
+// VERIFY STRIPE--->>
+const verifyStripe = async (req, res) => {
+
+    const { orderId, success, userId } = req.body;
+
+    try {
+        
+        if(success === 'true'){
+            await orderModel.findByIdAndUpdate(orderId, {payment: true});
+            await userModel.findByIdAndUpdate(userId, {cardData: {}})
+
+            res.json({
+                success: true,
+            })
+        } else {
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({
+                success: false
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
 // PLACING ORDER USING RAZORPAY METHOD--->>
 const placeOrderRazorpay = async (req, res) => {
 
