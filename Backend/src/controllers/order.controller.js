6842ce92995366e6c9fc0818c01d6fdffa772df7
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-11 17:17:38 +0530

    feat: implement Stripe payment processing in placeOrderStripe method

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -1,12 +1,20 @@
-import orderModel from '../models/order.model.js';
+import orderModel from '../models/order.model.js'
 import userModel from '../models/user.model.js'
+import Stripe from 'stripe'
+
+// GLOBAL VAR--->>
+const currency = 'inr'
+const deliveryCharges = 10
+
+// GATEWAY INITIALIZE--->>
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // PLACING ORDER USING COD METHOD--->>
 const placeOrder = async (req, res) => {
 
     try {
 
-        const { userId, items, amount, address, paymentMthod } = req.body;
+        const { userId, items, amount, address } = req.body;
 
         const orderData = {
             userId,
@@ -40,6 +48,66 @@ const placeOrder = async (req, res) => {
 
 // PLACING ORDER USING STRIPE METHOD--->>
 const placeOrderStripe = async (req, res) => {
+
+    try {
+
+        const { userId, items, amount, address } = req.body;  
+        const { origin } = req.headers
+        
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: 'Stripe',
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save()
+
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: 'Delivery Charges'
+                },
+                unit_amount: deliveryCharges * 100
+            },
+            quantity: 1
+        })
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`, 
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment'
+        })
+
+        res.json({
+            success: true,
+            session_url: session.url
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 
 }
 
