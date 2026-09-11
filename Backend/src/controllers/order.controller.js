d88db513c42aa31c11237c56bb868bde50bb4e2c
nitish yadav <yadavmunnakumar951@gmail.com>
2026-09-11 16:37:38 +0530

    feat: implement order status update functionality in updateStatus method

--- Backend/src/controllers/order.controller.js
+++ Backend/src/controllers/order.controller.js
@@ -95,6 +95,24 @@ const userOrders = async (req, res) => {
 // UPDATE ORDER STATUS FROM ADMIN PANEL--->>
 const updateStatus = async (req, res) => {
 
+    try {
+
+        const { orderId, status } = req.body;
+        await orderModel.findByIdAndUpdate(orderId, {status})
+
+        res.json({
+            success: true,
+            message: 'Status Updated'
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: true,
+            message: error.message
+        })
+    }
+
 }
 
 export { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus }
